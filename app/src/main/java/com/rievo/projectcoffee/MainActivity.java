c49dd96190b46706c9d6d00376d901f2556db038
Cara Ka Lai Yung Yung <cklyyung@uwaterloo.ca>
2017-11-06 13:53:03 -0500

    login viewgroup

--- app/src/main/java/com/rievo/projectcoffee/MainActivity.java
+++ app/src/main/java/com/rievo/projectcoffee/MainActivity.java
@@ -2,8 +2,11 @@ package com.rievo.projectcoffee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.rievo.library.BackStack;
+import com.rievo.library.BackStackManager;
+import com.rievo.library.LinearBackStack;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BackStack.install(this);
+
+        BackStackManager backStackManager = BackStack.getBackStackManager();
+
+        FrameLayout root = findViewById(R.id.root);
+
+        LinearBackStack linearBackStack = backStackManager.createLinearBackStack("TAG", root, (layoutInflater, container) -> {
+            //This is our first view group in the stack
+            ViewGroupLogin vg = (ViewGroupLogin) layoutInflater.inflate(R.layout.viewgroup_login, container, false);
+
+            //Make sure that the view is added to container by the end of this block
+            container.addView(vg);
+
+            //Return the view group that was newly inflated
+            return vg;
+        });
+
     }
 
     @Override
---
+++ app/src/main/java/com/rievo/projectcoffee/ViewGroupLogin.java
@@ -0,0 +1,30 @@
+package com.rievo.projectcoffee;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class ViewGroupLogin extends RelativeLayout {
+
+    public ViewGroupLogin(Context context) {
+        super(context);
+    }
+
+    public ViewGroupLogin(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ViewGroupLogin(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init(){
+
+    }
+
+    @Override
+    public void onAttachedToWindow () {
+        super.onAttachedToWindow();
+    }
+
+}
