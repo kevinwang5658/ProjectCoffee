4ed4ca82836a43620bec464318dbea0567032c49
Kevin Wang <kevinwang5658@gmail.com>
2017-11-06 13:04:57 -0500

    drawer

--- app/src/main/java/com/rievo/projectcoffee/MainActivity.java
+++ app/src/main/java/com/rievo/projectcoffee/MainActivity.java
@@ -3,11 +3,22 @@ package com.rievo.projectcoffee;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rievo.library.BackStack;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BackStack.install(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!BackStack.getBackStackManager().goBack()) {
+            super.onBackPressed();
+        }
     }
 }
