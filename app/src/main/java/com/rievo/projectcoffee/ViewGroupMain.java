bc2b715ebf8698d815b8d2afdd85db13fb16f60f
Kevin Wang <kevinwang5658@gmail.com>
2017-11-07 16:52:03 -0500

    animation

--- app/src/main/java/com/rievo/projectcoffee/ViewGroupMain.java
+++ app/src/main/java/com/rievo/projectcoffee/ViewGroupMain.java
@@ -1,19 +1,32 @@
 package com.rievo.projectcoffee;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.ViewAnimator;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.rievo.library.BackStack;
+import com.rievo.library.LinearBackStack;
 
 import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static com.rievo.projectcoffee.MainActivity.SP_TAG;
 
 /**
  * Created by kwang on 2017-11-06.
@@ -24,6 +37,7 @@ public class ViewGroupMain extends RelativeLayout {
     @BindView(R.id.points) TextView points;
     @BindView(R.id.drawer_top) RelativeLayout drawerTop;
     @BindView(R.id.drawer_text) TextView drawerText;
+    @BindView(R.id.view) View view;
 
     public ViewGroupMain(Context context) {
         super(context);
@@ -61,6 +75,44 @@ public class ViewGroupMain extends RelativeLayout {
     }
 
     @OnClick(R.id.view) public void onPointViewClick() {
-        ((MainActivity) getContext()).history();
+        ViewGroup.LayoutParams layoutParams1 = view.getLayoutParams();
+        /*LayoutParams layoutParams = new LayoutParams(3000, 3000);
+        layoutParams.setMargins(-600, -600, -600, -600);
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        view.setLayoutParams(layoutParams);
+        view.invalidate();
+        view.setElevation(100);*/
+
+
+
+        YoYo.with(Techniques.FadeOut)
+                .duration(100)
+                .onEnd(callback->{
+                    ((ViewGroup) view).removeView(view.findViewById(R.id.text_layout));
+                    view.animate().scaleXBy(20).scaleYBy(20).setDuration(400)
+                            .setListener(new Animator.AnimatorListener() {
+                                @Override
+                                public void onAnimationStart(Animator animation) {
+
+                                }
+
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    ((MainActivity) getContext()).history();
+                                }
+
+                                @Override
+                                public void onAnimationCancel(Animator animation) {
+
+                                }
+
+                                @Override
+                                public void onAnimationRepeat(Animator animation) {
+
+                                }
+                            })
+                            .start();
+                })
+                .playOn(findViewById(R.id.text_layout));
     }
 }
