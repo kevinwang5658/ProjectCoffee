95ce2d55da33be86ec44f741027720583a4b2a2b
Cara Ka Lai Yung Yung <cklyyung@uwaterloo.ca>
2017-11-08 15:28:54 -0500

    list view in the redeem page

--- app/src/main/java/com/rievo/projectcoffee/RedeemAdapter.java
+++ app/src/main/java/com/rievo/projectcoffee/RedeemAdapter.java
@@ -1,9 +1,11 @@
 package com.rievo.projectcoffee;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,59 +15,47 @@ import java.util.List;
  * Created by kwang on 2017-11-08.
  */
 
-public class RedeemAdapter extends RecyclerView.Adapter<RedeemAdapter.ViewHolder> {
+public class RedeemAdapter extends ArrayAdapter<String> {
 
-    List<String> list = new ArrayList<>();
-
-    {
-        list.add("Coffee");
-        list.add("Mug");
-        list.add("Menu Item");
+    public RedeemAdapter(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
     }
 
-    private OnClick onClick;
-
-    RedeemAdapter(OnClick listener){
-        this.onClick = listener;
+    public RedeemAdapter(Context context, int resource, List<String> items) {
+        super(context, resource, items);
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.redeem_item, parent, false);
-        return new ViewHolder(itemView, this);
-    }
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.textView.setText(list.get(position));
-        holder.position = position;
-    }
+        View v = convertView;
 
-    private void onClick(int position){
-        onClick.onClick(position);
-    }
-
-    @Override
-    public int getItemCount() {
-        return 3;
-    }
-
-    public static final class ViewHolder extends RecyclerView.ViewHolder {
-
-        TextView textView;
-        int position;
-
-        public ViewHolder(View itemView, RedeemAdapter adapter) {
-            super(itemView);
-            textView = itemView.findViewById(R.id.redeem_text);
-            itemView.setOnClickListener(v->{
-                adapter.onClick(position);
-                itemView.setBackgroundColor(0x4f4f4f);
-            });
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.redeem_item, null);
         }
+
+        String string = getItem(position);
+
+        if (string != null) {
+            TextView tt1 = (TextView) v.findViewById(R.id.redeem_text);
+            TextView tt2 = v.findViewById(R.id.redeem_points);
+
+            if (tt1 != null) {
+                tt1.setText(string);
+            }
+
+            if (tt2 != null) {
+                if (string == "Any Menu Item") {
+                    tt2.setText("200 points");
+                } else {
+                    tt2.setText("0 points");
+                }
+            }
+        }
+
+        return v;
     }
 
-    interface OnClick{
-        void onClick(int position);
-    }
 }
--- app/src/main/java/com/rievo/projectcoffee/ViewGroupRedeem.java
+++ app/src/main/java/com/rievo/projectcoffee/ViewGroupRedeem.java
@@ -8,12 +8,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -26,7 +28,7 @@ import butterknife.ButterKnife;
 public class ViewGroupRedeem extends LinearLayout {
 
     @BindView(R.id.redeem_listview)
-    RecyclerView listView;
+    ListView listView;
     @BindView(R.id.qr_image)
     ImageView qrImage;
 
@@ -46,12 +48,31 @@ public class ViewGroupRedeem extends LinearLayout {
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
 
-        ButterKnife.bind(this);
-        listView.setAdapter(new RedeemAdapter((position -> {
-            Log.d("TAG", position + "");
-        })));
-        listView.setLayoutManager(new LinearLayoutManager(getContext()));
+        List items = new ArrayList<String>();
 
+        items.add("Coffee");
+        items.add("Lighthouse Mug");
+        items.add("Any Menu Item");
+
+        ButterKnife.bind(this);
+        listView.setAdapter(new RedeemAdapter(getContext(), R.layout.redeem_item, items));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        qrImage.setImageDrawable(getResources().getDrawable(R.drawable.qr_coffee));
+                        break;
+                    case 1:
+                        qrImage.setImageDrawable(getResources().getDrawable(R.drawable.qr_mug));
+                        break;
+                    case 2:
+                        qrImage.setImageDrawable(getResources().getDrawable(R.drawable.qr_menu));
+                        break;
+                }
+            }
+        });
 
     }
 
