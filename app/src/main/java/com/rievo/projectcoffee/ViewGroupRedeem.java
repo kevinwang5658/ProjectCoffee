41ac7eb260e6ea2e792397825be1d820e3ed90d0
Cara Ka Lai Yung Yung <cklyyung@uwaterloo.ca>
2017-11-08 13:27:30 -0500

    images

--- app/src/main/java/com/rievo/projectcoffee/ViewGroupRedeem.java
+++ app/src/main/java/com/rievo/projectcoffee/ViewGroupRedeem.java
@@ -2,14 +2,30 @@ package com.rievo.projectcoffee;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by kwang on 2017-11-08.
  */
 
 public class ViewGroupRedeem extends LinearLayout {
+
+    @BindView(R.id.redeem_listview)
+    ListView listView;
+    @BindView(R.id.qr_image)
+    ImageView qrImage;
+
     public ViewGroupRedeem(Context context) {
         super(context);
     }
@@ -25,5 +41,9 @@ public class ViewGroupRedeem extends LinearLayout {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+
+
     }
+
 }
+
